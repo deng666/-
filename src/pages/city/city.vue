ce7f93a05e596068afcc1df48cba0c20eade99dd
wangdengdeng <273813650@qq.com>
2018-12-07 18:39:21 +0800

    微调

--- src/pages/city/city.vue
+++ src/pages/city/city.vue
@@ -2,7 +2,7 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
-    <city-list></city-list>
+    <city-list :cityChild="clityDetail"></city-list>
     <city-alphabet></city-alphabet> 
   </div>
 </template>
@@ -12,14 +12,40 @@ import cityHeader from './components/header'
 import cityList from './components/list'
 import citySearch from './components/search'
 import cityAlphabet from './components/alphabet'
+import axios from 'axios'
 export default {
   name: 'city',
+  data () {
+    return {      
+      clityDetail: {
+        local: '',
+        hotCity: [],
+        alphabetList: [],
+        // provincial: []
+      }
+    }
+  },
   components: {
     cityHeader,
     cityList,
     citySearch,
     cityAlphabet
   },
+  methods: {
+    getCityInfo() {
+      axios.get('/api/city.json').then(res => {
+        res = res.data
+        if (res.ret && res.data) {
+          debugger
+          let data = res.data
+          // this.clityDetail.local = data.local
+        }
+      })
+    }
+  },
+  mounted () {
+    this.getCityInfo()
+  }
 }
 </script>
 
